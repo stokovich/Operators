e96ad39a8d18c54fdeafc1b0a66c54ef6f9c9451
Stoyan Penchev <spenchev@gmail.com>
2017-08-02 13:58:27 +0300

    before trapezoids

---
+++ PointInCircle/PointInCircle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PointInCircle
+{
+    static void Main()
+    {
+        decimal a = decimal.Parse(Console.ReadLine());
+        decimal b = decimal.Parse(Console.ReadLine());
+
+        decimal c = (decimal)Math.Sqrt((double)(a * a + b * b));
+
+        if (c <= 2)
+        {
+            Console.WriteLine("yes {0:0.00}",c);
+        }
+        else
+        {
+            Console.WriteLine("no {0:0.00}", c);
+        }
+    }
+}
---
+++ PrimeCheck/PrimeCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class PrimeCheck
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        double squareN = Math.Sqrt(N);
+        string result = "true";
+
+        if (N > 0 && N != 1)
+        {
+            for (int i = 2; i < N; i++)
+            {
+
+                if ((N % i) == 0)
+                {
+                    result = "false";
+                }
+            }
+        }
+        else
+        {
+            result = "false";
+        }
+        Console.WriteLine(result);
+    }
+}
