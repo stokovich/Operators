29d09a1330e9aa605f74d8302d4b0ba64572167f
Stoyan Penchev <spenchev@gmail.com>
2017-08-03 09:33:07 +0300

    before N-th Bit excercise

---
+++ CicleRectangleIO/CicleRectangleIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CicleRectangleIO
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        string rescircle;
+        string resrectangle;
+
+        if ( ( Math.Pow(x - 1,2) + Math.Pow(y - 1,2) ) <= Math.Pow(1.5,2))
+        {
+            rescircle = "inside circle";
+        }
+        else
+        {
+            rescircle = "outside circle";
+        }
+
+        if ( x >= -1 && x <= 5 && y >= -1 && y <= 1)
+        {
+            resrectangle = "inside rectangle";
+        }
+        else
+        {
+            resrectangle = "outside rectangle";
+        }
+        Console.WriteLine("{0} {1}",rescircle,resrectangle);
+    }
+}
---
+++ ThirdBit/ThirdBit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ThirdBit
+{
+    static void Main()
+    {
+        int Position = 3;
+        int Mask = 1 << Position;
+        int Number = int.Parse(Console.ReadLine());
+        int NumberAndMask = Number & Mask;
+        int Result = NumberAndMask >> Position;
+
+        Console.WriteLine(Result);
+    }
+}
